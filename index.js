483b94c46a120eeb0a5b5c850468a19865d788cf
Kayleigh Hanna <kayleighchanna@gmail.com>
2021-06-20 16:20:24 -0500

    added filepath to output

--- index.js
+++ index.js
@@ -6,13 +6,11 @@ const fs = require("fs")
 const OUTPUT_DIR = path.resolve(__dirname, "output")
 const outputPath = path.join(OUTPUT_DIR, "team.html")
 const render = require("./src/pagetemp.js");//render req page temp output to team.html
-const Manager = require("./lib/Manager");
-const Intern = require("./lib/Intern");
-const Engineer = require("./lib/Engineer");
 
 
 const buildProfile = (team) => {
     fs.writeFileSync(outputPath, render.generateHtml(team));
+    console.log(`File written to ${outputPath}`);
     process.exit()
 }
 
